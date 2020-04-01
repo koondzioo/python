51f172f1e591f3dbd66c686de74c884d3ee67934
Konrad Oleksiak <Konrad.Oleksiak@orange.com>
2020-04-01 23:22:54 +0200

    p4

--- projekt_4/app.py
+++ projekt_4/app.py
@@ -1,11 +1,6 @@
-# północ (N), południe (S), wschód (E), zachód (W).
-kierunki = ['N', 'E', 'S', 'W']
-ruchy = {'M': 1,
-         'R': 1,
-         'L': -1}
-
 
 def create_board(size):
+    """Function to create board"""
     board = []
     for idx in range(size):
         board.append(['-' for _ in range(size)])
@@ -15,46 +10,95 @@ def create_board(size):
 
 
 def print_board(board):
+    """Function to print player board"""
     for line in board:
         print(line)
 
 
 def set_start_point(board):
+    """Function to set start point"""
     board[len(board[0]) - 1][0] = 'x'
-    return board
+    return len(board[0]) - 1, 0, 'N'
 
 
-def set_point(board, idx, idy):
+def set_point(board, points):
+    idx, idy, current_direction = points
+    """Function set current point in board"""
     board[idx][idy] = 'x'
-    return board
+    return idx, idy, current_direction
 
 
-def move(board, points, string_move):
-    idx, idy, kierunek = points
+def move(board, points, string_move, directions):
+    idx, idy, current_direction = points
     board[idx][idy] = '-'
-    global kierunki, ruchy
     moves = [m for m in string_move]
     for move_player in moves:
-        if kierunek == 'N' and move_player == 'M':
-            idx -= 1
-        if move_player == 'R':
-            idx_kierunki = kierunki.index(kierunek)
-            kierunek = kierunki[idx_kierunki+1]
-            print(kierunek)
+        if move_player in ['L', 'R']:
+            current_direction = check_direction(move_player, current_direction, directions)
+        else:
+            idx, idy = move_forward(len(board), idx, idy, current_direction)
+    return idx, idy, current_direction
+
+
+def move_forward(size, idx, idy, current_direction):
+    """Function check index next move"""
+    if current_direction == 'N':
+        idx -= 1
+        if idx < 0:
+            idx = 0
+    if current_direction == 'S':
+        idx += 1
+        if idx > size - 1:
+            idx = size - 1
+    if current_direction == 'E':
+        idy += 1
+        if idy >= size - 1:
+            idy = size - 1
+    if current_direction == 'W':
+        idy -= 1
+        if idy < 0:
+            idy = 0
     return idx, idy
 
 
+def check_direction(move_player, current_direction, directions):
+    """Function check next direction"""
+    if move_player == 'R':
+        id_direction = directions.index(current_direction)
+        if id_direction == len(directions) - 1:
+            current_direction = directions[0]
+        else:
+            current_direction = directions[id_direction + 1]
+    if move_player == 'L':
+        id_direction = directions.index(current_direction)
+        if id_direction == 0:
+            current_direction = directions[-1]
+        else:
+            current_direction = directions[id_direction - 1]
+    return current_direction
+
+
+def check_win(board):
+    """function checks if you won"""
+    return board[0][len(board)-1] == 'x'
+
+def main():
+    """Main game function"""
+    directions = ['N', 'E', 'S', 'W']
+    board = create_board(4)
+    points = set_start_point(board)
+    while True:
+        print_board(board)
+        string_move = input('Insert string: ')
+        idx, idy, current_direction = move(board, points, string_move, directions)
+        points = idx, idy, current_direction
+        set_point(board, points)
+        if check_win(board):
+            print('You win!')
+            break
+        print(f'current_direction {current_direction}')
+
 
 if __name__ == '__main__':
-    board = create_board(4)
-    set_start_point(board)
-    print_board(board)
-    print('-----------')
-    points = 3, 0, kierunki[0]
-    idx, idy = move(board, points, 'MMRR')
-    set_point(board, idx, idy)
-    print_board(board)
+    main()
 
-    # board = create_board(4)
-    # set_start_point(board)
-    # print_board(board)
